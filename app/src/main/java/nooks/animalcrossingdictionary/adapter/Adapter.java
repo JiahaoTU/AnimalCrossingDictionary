522a82086b4e6841912437d406343426b4b40e07
xinyi-huang96 <xinyi.huang96@hotmail.com>
2020-12-08 20:09:20 +0100

    fish recyclelist

--- app/src/main/java/nooks/animalcrossingdictionary/adapter/Adapter.java
+++ app/src/main/java/nooks/animalcrossingdictionary/adapter/Adapter.java
@@ -1,9 +1,9 @@
 package nooks.animalcrossingdictionary.adapter;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,12 +20,19 @@ public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public static class MyViewHolder_fish extends RecyclerView.ViewHolder {
 
         public TextView name;
-        public TextView shadow;
+        public TextView size, loc, price, month, rarity, time;
+        public ImageView icon;
 
         public MyViewHolder_fish(View view) {
             super(view);
             name = view.findViewById(R.id.name);
-            shadow = view.findViewById(R.id.shadow);
+            size = view.findViewById(R.id.size_info);
+            loc = view.findViewById(R.id.location_info);
+            price = view.findViewById(R.id.price_info);
+            month = view.findViewById(R.id.month_info);
+            rarity = view.findViewById(R.id.rarity_info);
+            time = view.findViewById(R.id.time_info);
+            icon = view.findViewById(R.id.imageView);
         }
     }
 
@@ -46,7 +53,10 @@ public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ((MyViewHolder_fish)holder).name.setText(listData.get(position).getName().getNameEUen());
-        ((MyViewHolder_fish)holder).shadow.setText(listData.get(position).getShadow().split(" ")[0]);
+        ((MyViewHolder_fish)holder).size.setText(listData.get(position).getShadow().split(" ")[0]);
+        ((MyViewHolder_fish)holder).loc.setText(listData.get(position).getAvailability().getLocation());
+        ((MyViewHolder_fish)holder).price.setText(listData.get(position).getPrice()+"");
+        ((MyViewHolder_fish)holder).rarity.setText(listData.get(position).getAvailability().getRarity());
 
     }
 
